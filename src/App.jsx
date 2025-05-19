79ac15b0e1736e182c1b044b2f6ab29aba694d85
Narek Zakarian <narekzakarian@gmail.com>
2025-05-19 18:14:36 +0400

    Use the TailwindCss and config it

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,11 @@
-import {useEffect, useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-const Card = ({ title }) => {
-    const [count, setCount] = useState(0)
-    const [hasLiked, setHasLiked] = useState(false);
-    
-    useEffect(() => {
-        console.log(`${title} has been liked: ${hasLiked}`)
-    }, [hasLiked]);
-    
-    return (
-        <div className="card" onClick={() => setCount(count + 1)}>
-            <h2>{title} <br/> {count || null}</h2>
-            
-            <button onClick={() => setHasLiked(!hasLiked)}>
-                {hasLiked ? '❤️' : '🤍'}
-            </button>
-        </div>
-    )
-}
+import React from "react";
 
 const App = () => {
-    return (
-        <div className="card-container">
-            <Card title="Star Wars"/>
-            <Card title="Avatar"/>
-            <Card title="The Lion King"/>
+    return(
+        <div>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
         </div>
     )
 }
