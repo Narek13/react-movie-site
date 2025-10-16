1036335474860d80251572de2184e29bfc56ff85
Narek Zakarian <narekzakarian@gmail.com>
2025-10-16 16:16:58 +0400

    feat: Create the "Search" component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
+import Search from "./components/Search.jsx";
 
 const App = () => {
+    const [searchTerm, setSearchTerm] = useState('asd')
     return(
-        <div>
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
-        </div>
+        <main>
+            <div className="pattern" />
+            
+            <div className="wrapper" >
+                <header>
+                    <img src="/hero.png" alt="Hero Banner" />
+                    <h1>
+                        Find <span className="text-gradient">Movies</span> you'll enjoy without hassle
+                    </h1>
+                </header>
+                
+                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+                <h1 className="text-white">{searchTerm}</h1>
+            </div>
+        </main>
     )
 }
 
---
+++ src/components/Search.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Search({ searchTerm, setSearchTerm }) {
+    return (
+        <div className="search">
+            <div>
+                <img src="/search.svg" alt="Search" />
+                
+                <input
+                    type="text"
+                    placeholder="Search through thousand of moovies"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+            </div>
+        </div>
+    );
+}
+
+export default Search;
