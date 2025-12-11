98fa121396788ec7fd63b792328d6e1c5a3a4be0
Narek Zakarian <narekzakarian@gmail.com>
2025-12-11 18:51:36 +0400

    feat: Add appWrite and update search movie count logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {useDebounce} from "react-use";
 import Search from "./components/Search.jsx";
 import Spinner from "./components/Spinner.jsx";
 import MovieCard from "./components/MovieCard.jsx";
+import {updateSearchCount} from "./appwrite.js";
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -21,7 +22,7 @@ const App = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
     
-    useDebounce( () => setDebouncedSearchTerm(searchTerm), 500, [searchTerm] )
+    useDebounce( () => setDebouncedSearchTerm(searchTerm), 1000, [searchTerm] )
     
     const fetchMovies = async (query = '') => {
         setIsLoading(true);
@@ -48,6 +49,9 @@ const App = () => {
             
             setMovieList(data.results || []);
             
+            if (query && data.results.length > 0) {
+                await updateSearchCount(query, data.results[0]);
+            }
         } catch (error) {
             console.error(`Error fetching movies: ${error}`);
             setErrorMessage('Error fetching movies. Please try again later')
